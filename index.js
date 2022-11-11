5ee9ed48f385f3539785fc933430eaaa204fdded
Keian Anthony <KeianAnthony1@gmail.com>
2022-11-10 20:05:59 -0800

    cors config

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 
 // environment variables
